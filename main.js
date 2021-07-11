cc0bd6a4765f13e74e9323afbac4f4b184f9122c
FireRubies <40702001+FireRubies@users.noreply.github.com>
2021-07-11 11:56:50 -1000

    Removed some redundant files.

--- main.js
+++ main.js
@@ -2499,3 +2499,21 @@ function displayConfetti(seconds) {
     startConfetti();
     setTimeout(stopConfetti, (seconds * 1000));
 }
+
+//Audio
+
+var audio = new Audio("http://soundimage.org/wp-content/uploads/2017/05/Puzzle-Dreams-2.mp3");
+
+    audio.addEventListener('ended', function() {
+        this.currentTime = 1;
+        this.play();
+    }, false);
+
+
+function musicButton() {
+  if (audio.paused) {
+    audio.play();
+  } else {
+    audio.pause();
+  }
+}
